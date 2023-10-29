9ca1ea01f46f1f29745e73ba2b3094202dce7b2b
Valentín Veintemilla <valentinveintemilla@gmail.com>
2023-10-29 20:20:15 -0300

    Subimos algunos cambios

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express"); // Importa ExpressJS. Más info de Express en =>https://expressjs.com/es/starter/hello-world.html
 
+const mariadb = require('mariadb'); // Importamos la libreria "MariaDB"
+
+// Permite crear las conexiones a la base de datos
+const pool = mariadb.createPool({host: "localhost", user: "root", password:"1234", database:"pruebadb", connectionLimit: 5});
+
 const app = express(); // Crea una instancia de ExpressJS
 
 const port = 3000;
@@ -13,8 +18,28 @@ app.get("/", (req, res) => {
   res.send("<h1>Bienvenid@ al servidor</h1>");
 });
 
-app.get("/people", (req, res) => {
-  res.json(people); // Enviamos todo el array
+// app.get("/people", (req, res) => {
+//   res.json(people); // Enviamos todo el array
+// });
+
+// Utilizacion de la base de datos desde nodeJS
+
+app.get("/people", async (req, res) => {
+  let conn;
+  try {
+
+  // Realiza el select de la database
+  conn = await pool.getConnection();
+  const rows = await conn.query("SELECT name, lastname, email FROM people");
+
+  // Linea que contesta al cliente
+  res.json(rows);
+
+  }catch(error){
+    res.status(500).json({message:"Se rompió el servidor"});
+  } finally {
+  if (conn) conn.release(); //release to pool
+  }
 });
 
 app.get("/people/:index", (req, res) => {
@@ -25,14 +50,35 @@ app.get("/people/:index", (req, res) => {
   res.json(people[req.params.index]); // Enviamos el elemento solicitado por su índice
 });
 
-app.post("/people", (req, res) => {
-  /* La propiedad "body" del request permite acceder a los datos 
-       que se encuentran en el cuerpo de la petición */
+// app.post("/people", (req, res) => {
+//   /* La propiedad "body" del request permite acceder a los datos 
+//        que se encuentran en el cuerpo de la petición */
 
-  people.push(req.body); // Añadimos un nuevo elemento al array
+//   people.push(req.body); // Añadimos un nuevo elemento al array
 
-  res.json(req.body); // Le respondemos al cliente el objeto añadido
+//   res.json(req.body); // Le respondemos al cliente el objeto añadido
+// });
+
+// Intento realizar la funcion post con nodeJS
+
+app.post("/people", async (req, res) => {
+  let conn;
+  try {
+
+  // Realiza el select de la database
+  conn = await pool.getConnection();
+  const rows = await conn.query("SELECT name, lastname, email FROM people");
+
+  // Linea que contesta al cliente
+  res.json(rows);
+
+  }catch(error){
+    res.status(500).json({message:"Se rompió el servidor"});
+  } finally {
+  if (conn) conn.release(); //release to pool
+  }
 });
+
 
 app.put("/people/:index", (req, res) => {
   /* COMPLETA EL CÓDIGO NECESARIO:
